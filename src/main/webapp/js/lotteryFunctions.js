4b2e6338a0fbd91c8e4e5942dcd9cfc4703ff381
Francesco Maglia <Francesco.Maglia@IGT.com>
2017-04-21 23:46:56 +0200

    Added logos combination. Modified frontend

--- src/main/webapp/js/lotteryFunctions.js
+++ src/main/webapp/js/lotteryFunctions.js
@@ -1,10 +1,15 @@
 function restCall () {
     $.ajax({
         type: "GET",
-        dataType: "jsonp",
+        dataType: "json",
         url: "http://localhost:8080/lottery/purchaseTicket",
         success: function(data){
-            alert(data);
+            $("#a1").attr("src", "img/"+data.drawnNumbers[0]+".png?150x80");
+            $("#a2").attr("src", "img/"+data.drawnNumbers[1]+".png?150x80");
+            $("#a3").attr("src", "img/"+data.drawnNumbers[2]+".png?150x80");
+            $("#b1").attr("src", "img/"+data.drawnNumbers[3]+".png?150x80");
+            $("#b2").attr("src", "img/"+data.drawnNumbers[4]+".png?150x80");
+            $("#b3").attr("src", "img/"+data.drawnNumbers[5]+".png?150x80");
         }
     });
 }
