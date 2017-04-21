7f36af31e103d5c88aaa1e27d71c1c600d83388f
Francesco Maglia <Francesco.Maglia@IGT.com>
2017-04-22 00:19:48 +0200

    Cosmetic changes

--- src/main/webapp/js/lotteryFunctions.js
+++ src/main/webapp/js/lotteryFunctions.js
@@ -25,7 +25,6 @@ function restCall () {
                 $("#d5").attr("class", "panel panel-danger");
                 $("#d6").attr("class", "panel panel-danger");
             }
-
         }
     });
 }
