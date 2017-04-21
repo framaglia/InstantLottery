9b06df7a70c63f1608446e611fff5d1db19af003
Francesco Maglia <Francesco.Maglia@IGT.com>
2017-04-22 00:18:33 +0200

    Style modification

--- src/main/webapp/js/lotteryFunctions.js
+++ src/main/webapp/js/lotteryFunctions.js
@@ -10,6 +10,28 @@ function restCall () {
             $("#b1").attr("src", "img/"+data.drawnNumbers[3]+".png?150x80");
             $("#b2").attr("src", "img/"+data.drawnNumbers[4]+".png?150x80");
             $("#b3").attr("src", "img/"+data.drawnNumbers[5]+".png?150x80");
+            if(data.winning == true) {
+                $("#d1").attr("class", "panel panel-success");
+                $("#d2").attr("class", "panel panel-success");
+                $("#d3").attr("class", "panel panel-success");
+                $("#d4").attr("class", "panel panel-success");
+                $("#d5").attr("class", "panel panel-success");
+                $("#d6").attr("class", "panel panel-success");
+            } else {
+                $("#d1").attr("class", "panel panel-danger");
+                $("#d2").attr("class", "panel panel-danger");
+                $("#d3").attr("class", "panel panel-danger");
+                $("#d4").attr("class", "panel panel-danger");
+                $("#d5").attr("class", "panel panel-danger");
+                $("#d6").attr("class", "panel panel-danger");
+            }
+
         }
     });
 }
+
+$("#button").click(function() {
+    $('html,body').animate({
+            scrollTop: $(".second").offset().top},
+        'slow');
+});
