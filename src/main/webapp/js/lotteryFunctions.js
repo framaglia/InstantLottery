10cefb7428855f823220927548d2cedd3937d908
Francesco Maglia <Francesco.Maglia@IGT.com>
2017-04-23 22:20:02 +0200

    GoT themed frontend

--- src/main/webapp/js/lotteryFunctions.js
+++ src/main/webapp/js/lotteryFunctions.js
@@ -4,12 +4,12 @@ function restCall () {
         dataType: "json",
         url: "http://localhost:8080/lottery/purchaseTicket",
         success: function(data){
-            $("#a1").attr("src", "img/"+data.drawnNumbers[0]+".png?150x80");
-            $("#a2").attr("src", "img/"+data.drawnNumbers[1]+".png?150x80");
-            $("#a3").attr("src", "img/"+data.drawnNumbers[2]+".png?150x80");
-            $("#b1").attr("src", "img/"+data.drawnNumbers[3]+".png?150x80");
-            $("#b2").attr("src", "img/"+data.drawnNumbers[4]+".png?150x80");
-            $("#b3").attr("src", "img/"+data.drawnNumbers[5]+".png?150x80");
+            $("#a1").attr("src", "img/"+data.drawnNumbers[0]+".gif?150x80");
+            $("#a2").attr("src", "img/"+data.drawnNumbers[1]+".gif?150x80");
+            $("#a3").attr("src", "img/"+data.drawnNumbers[2]+".gif?150x80");
+            $("#b1").attr("src", "img/"+data.drawnNumbers[3]+".gif?150x80");
+            $("#b2").attr("src", "img/"+data.drawnNumbers[4]+".gif?150x80");
+            $("#b3").attr("src", "img/"+data.drawnNumbers[5]+".gif?150x80");
             if(data.winning == true) {
                 $("#d1").attr("class", "panel panel-success");
                 $("#d2").attr("class", "panel panel-success");
